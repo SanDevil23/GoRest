1472cf1ae396978f19683a13fb35e78bc9d68221
SanDevil23 <sankalpadhya05@gmail.com>
2025-03-03 11:24:30 +0530

    initial commit

---
+++ src/rest/go.mod
@@ -0,0 +1,5 @@
+module rest
+
+go 1.24.0
+
+require github.com/gorilla/mux v1.8.1
---
+++ src/rest/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// declaring the structure for the article
+type Article struct {
+	Id 		string `json:"id"`
+	Title 	string `json:"title"`
+	Desc 	string `json:"desc"`
+	Content string `json:"content"`
+}
+
+// array of Article for storing articles
+var articles []Article;
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homepage")
+}
+
+func handleRequests() {
+
+	// handler
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/delete/{id}", deleteArticle).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":3000", myRouter))
+	fmt.Println("Server successfully started on the port: 3000" )
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	key := vars["id"]
+	// fmt.Fprintln(w, "Key: " + key)
+
+	// Loop over all of our Articles
+    // if the article.Id equals the key we pass in
+    // return the article encoded as JSON
+    for _, article := range articles {
+        if article.Id == key {
+            json.NewEncoder(w).Encode(article)
+        }
+    }
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request){
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	var art Article
+	json.Unmarshal(reqBody, &art)
+	// fmt.Fprintf(w, "%+v", string(reqBody))
+
+	// update our global variable of articles
+	// to include the new article
+
+	articles = append(articles, art)
+	json.NewEncoder(w).Encode(art)
+}
+
+
+func deleteArticle(w http.ResponseWriter, r *http.Request){
+	// parsing the path parameters to read the incoming request
+	vars := mux.Vars(r)
+
+	// here we are extracting the 'id' of the article passed in the path
+	id := vars["id"]
+
+	for index, article := range articles {
+		if article.Id == id {
+			articles = append(articles[:index], articles[index+1:]...)
+		}
+	}	
+	fmt.Println("The article is deleted", id)
+	fmt.Fprintln(w, "The Article has been deleted successfully")
+}
+
+func main() {
+
+	articles = []Article{
+		Article{Id:"1", Title: "Hello", Desc: "Protcol Buffers", Content: "Article Content"},
+		Article{Id:"2", Title: "Hello2", Desc: "Protcol Buffers2", Content: "Article Content2"},
+	}
+	handleRequests();
+}
